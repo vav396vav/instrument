8f34bd4c7c86ee6d9f5890d35e7d13e107c65f57
vav396vav <vav396vav@gmail.com>
2026-02-27 15:10:45 +0300

    создание анатаций (#3)

--- src/main/java/module4_reflection/annotation/part1/Main.java
+++ src/main/java/module4_reflection/annotation/part1/Main.java
@@ -1,13 +1,17 @@
 package module4_reflection.annotation.part1;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 
-public class Main {
+
+@Slf4j public class Main {
     public static void main(String[] args) throws NoSuchMethodException {
         Class<Processor> processorClass = Processor.class;
         Method doProcessMethod = processorClass.getMethod("doProcess");
+        System.out.println("Проверка");
 
         Annotation[] declaredAnnotations = doProcessMethod.getDeclaredAnnotations();
 
